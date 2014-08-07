a2c5f3e5e0f3029567bc41cdb835410ef3f828ec
motorolja <rasmusholm957@hotmail.com>
2014-08-07 20:44:27 +0200

    rr

---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import Request
+
+app = Flask(__name__)
+@app.route("/") 
+def root():
+    return "hej"
+
+@app.route("/<name>/hej") 
+def hej(name):
+    return "nooo " + name
+
+@app.route("/say_hej") 
+def say_hej():
+    name = request.args.get("name")
+    return name
+
+
+if __name__ == "__main__":
+    app.run()
